29250d94abc378a5d87327c6105ca942b9d23584
Casen Hunger <casen.h@utexas.edu>
2014-06-12 20:23:24 -0500

    Almost finished baseline simulator

--- Baseline_Simulator.cpp
+++ Baseline_Simulator.cpp
@@ -10,8 +10,9 @@ using namespace std;
 
 /* Some definitions to change simulation parameters */
 #define NUM_BANKS 8
-#define NUM_TRACES 1
-string TRACE_LOCATION("../../traces/LTE/dsp_0_trace.txt");
+#define NUM_TRACES 6
+#define MEM_DELAY 3
+string TRACE_LOCATION("/home/casen/Huawei/traces/LTE/dsp_0_trace.txt");
 
 
 /* Struct for the input requests from the processors */
@@ -40,8 +41,12 @@ typedef struct bank_request {
 /* GLOBALS */
 vector<bank_request> bank_reads[NUM_BANKS]; //Queue of read requests for each bank
 vector<bank_request> bank_writes[NUM_BANKS]; //Queue of write requests for each bank
+vector<request> request_queue[NUM_TRACES]; 
 int current_time = 0; //Current time in ns
-
+long long int critical_word_latency = 0;
+long long int last_word_latency = 0;
+long long int num_requests = 0;
+int mem_stall[NUM_BANKS];
 
 /**
  * This function takes an input line from the trace file, and populates the 
@@ -84,7 +89,7 @@ request parse_input(string input_command) {
 } 
 
 
-void get_requests(vector<request> request_queue[]) {
+void get_requests() {
 
 	/* Continue to read in commands until the end of file */
 	ifstream inputFile;
@@ -102,6 +107,7 @@ void get_requests(vector<request> request_queue[]) {
 		while(getline(inputFile, command)) {
 			request current_request = parse_input(command);
 			request_queue[i].push_back(current_request);
+			num_requests++;
 		}
 
 		/* Move to the next file name */
@@ -112,20 +118,25 @@ void get_requests(vector<request> request_queue[]) {
 
 
 int previous_size = 0;
-bool queue_empty(vector<request> request_queue[]) {
+bool queue_empty() {
 
 
-	if(request_queue[0].size() != previous_size) {
+	/*if(request_queue[0].size() != previous_size && request_queue[0].size() != 0) {
 		cout << "Size: " << previous_size << endl;
 		previous_size = request_queue[0].size();
 		cout << "Time: " << request_queue[0][0].time << endl;
-	}
+	}*/
 
 	for(int i = 0; i < NUM_TRACES; i++) {
 		if(request_queue[i].size() != 0) {
 			//cout << "Size: " << request_queue[i].size() << endl;
 			return false;
 		}
+	}
+
+	for(int i = 0; i < NUM_BANKS; i++) {
+		if(bank_reads[i].size() != 0 || bank_writes[i].size() != 0)
+			return false;
 	}
 
 	return true;
@@ -136,14 +147,15 @@ void input_controller(vector<request> request_queue[]) {
 
 	/* Check to see which requests need to be served */
 	vector<request> temp_requests;
+	bool roll_back_time = false;
 	for(int i = 0; i < NUM_TRACES; i++) {
 
 		//cout << "Request: " << request_queue[i][0].time << endl;
 		/* If it's time to serve the request, add it to the pending request queue */
-		if(request_queue[i][0].time == current_time) {
+		if(request_queue[i][0].time == current_time && !request_queue[i].empty()) {
 			temp_requests.push_back(request_queue[i][0]);
 			request_queue[i].erase(request_queue[i].begin()); //Remove the request from the queue
-			current_time -= 1; //Subtract one, since reads/writes can occur simultaneously in the trace files
+			roll_back_time = true;
 		}
 	}
 
@@ -172,7 +184,7 @@ void input_controller(vector<request> request_queue[]) {
 		next_request.last = false;
 
 		/* Determine which bank it falls into */
-		int bank = (pending_requests[0].address/32) % 8;
+		int bank = (pending_requests[0].address/32) % NUM_BANKS;
 
 		/* Determine if read/write queue entry */
 		if(pending_requests[0].read)
@@ -181,7 +193,7 @@ void input_controller(vector<request> request_queue[]) {
 			bank_writes[bank].push_back(next_request);
 
 		/* Now populate the next bank queues */
-		for(int n = 0; n < pending_requests[0].length; n++) {
+		for(int n = 1; n < pending_requests[0].length; n++) {
 
 			/* Update the next bank and make the next bank queue object */
 			if((++bank) >= NUM_BANKS)
@@ -190,9 +202,9 @@ void input_controller(vector<request> request_queue[]) {
 			next_request.read = pending_requests[0].read;
 			next_request.critical = false;
 			if(n == pending_requests[0].length - 1)
-				next_request.last = false;
-			else
 				next_request.last = true;
+			else
+				next_request.last = false;
 
 			/* Determine if read/write queue entry */
 			if(pending_requests[0].read)
@@ -201,24 +213,78 @@ void input_controller(vector<request> request_queue[]) {
 				bank_writes[bank].push_back(next_request);
 		}
 	}
+
+	if(roll_back_time == true)
+		current_time--;
 }
 
 
 
+void access_scheduler() {
+
+	/* Serve a request from each bank */
+	for(int i = 0; i < NUM_BANKS; i++) {
+
+		/* Serve a request from the greater queue */
+		if(bank_reads[i].size() >= bank_writes[i].size() && bank_reads[i].size() != 0 && mem_stall[i] == -1) {
+			if(bank_reads[i][0].critical == true) {
+				critical_word_latency += (current_time+1) - bank_reads[i][0].time;
+			}
+			if(bank_reads[i][0].last == true) {
+				last_word_latency += (current_time+1) - bank_reads[i][0].time;
+			}
+			bank_reads[i].erase(bank_reads[i].begin());
+			mem_stall[i] = 0;
+		}
+		else if(bank_writes[i].size() > bank_reads[i].size() && mem_stall[i] == -1) {
+			if(bank_reads[i][0].critical == true) {
+				critical_word_latency += (current_time+1) - bank_writes[i][0].time;
+			}
+			if(bank_reads[i][0].last == true) {
+				last_word_latency += (current_time+1) - bank_writes[i][0].time;
+			}	
+			bank_writes[i].erase(bank_writes[i].begin());	
+			mem_stall[i] = 0;
+		}
+	}
+
+	/* Add one clock cycle for each bank latency */
+	for(int i = 0; i < NUM_BANKS; i++) {
+
+		/* Reset the counter if the latency is reached */
+		if(mem_stall[i] == MEM_DELAY)
+			mem_stall[i] = -1;
+		/* Add one to the clock cycle if the memory is stalling */
+		if(mem_stall[i] >= 0)
+			mem_stall[i]++;
+	}
+}
+
+
 int sc_main(int argc, char* argv[]) {
 
+	/* Make sure the stalls start at 0 */
+	for(int i = 0; i < NUM_BANKS; i++) {
+		mem_stall[i] = 0;
+	}
+
 	/* First populate the request queues with all requests from banks */
-	vector<request> request_queue[NUM_TRACES]; 
-	get_requests(request_queue);
+	get_requests();
 	previous_size = request_queue[0].size();
 
 	/* Execute the main loop which will service all requests */
-	while(!queue_empty(request_queue)) {
+	while(!queue_empty()) {
 
 		input_controller(request_queue);
+		access_scheduler();
 
 		current_time += 1; //Cycle the clock 
+		cout << current_time << endl;
 	}
+
+	/* Dispaly the results */
+	cout << "Average critical word latency: " << critical_word_latency/num_requests << endl;
+	cout << "Average last word latency: " << last_word_latency/num_requests << endl;	
 }
 
 
