78857da60e740bd2fed7eac86b228ab2ed6f1628
hardikbjain <hardikbjain@gmail.com>
2014-08-09 05:33:47 -0500

    Changing run_test.sh for baseline

--- BaselineSimulator/run_tests.sh
+++ BaselineSimulator/run_tests.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-for i in {1..10}
-do
-	./sim $i "../traces/LTE/dsp_0_trace.txt"
-done
-for i in {1..10}
-do
-	./sim $i "../traces/UMTS/dsp_0_trace.txt"
+if [ "$#" -eq 0 ];then
+	echo "runtest usage :"
+	echo "./runtest.sh 1"
+	echo "where 1 is number of traces you want to run "
+	exit 0
+fi
+
+TRACENAME=("LTE" "UMTS" "case4" "trace1" "trace2" "trace3" "trace4")
+
+for ((j=0; j < $1 ; j ++))
+    do
+        for n in {1..10}
+        do
+	    ./sim $n "../traces/"${TRACENAME[j]}"/dsp_0_trace.txt"
+    done
 done
