c5e1200de4d63d950eee05aa98067edd0c809228
Casen Hunger <casen.h@utexas.edu>
2014-08-12 15:42:17 -0500

    Adding a new coding simulator to the repo. It fixes all the issues that Huawei brought up at the meeting.

--- CodingSimulator/run_tests.sh
+++ CodingSimulator/run_tests.sh
@@ -7,3 +7,19 @@ do
 		./sim $n $[i*5] 0 "../traces/UMTS/dsp_0_trace.txt" 
 	done
 done
+
+for i in {2..2}
+do
+	for n in {1..10}
+	do
+		./sim $n $[i*5] 0 "../traces/LTE/dsp_0_trace.txt" 
+	done
+done
+
+for i in {2..2}
+do
+	for n in {1..10}
+	do
+		./sim $n $[i*5] 0 "../traces/case4/dsp_0_trace.txt" 
+	done
+done
